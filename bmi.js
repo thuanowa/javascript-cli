56df4136e06ac4517d7b4b1a112e9f04bb7a399d
Thuan Pham <phamtanthuan2311@gmail.com>
2022-05-30 18:13:33 +0700

    [👌Auto commit] Saint Pipeline, please give me the green light

--- bmi.js
+++ bmi.js
@@ -7,20 +7,31 @@ const BMI = Math.round(KG / M ** 2);
 
 console.log("Your BMI is: " + BMI);
 
-if (BMI >= 0 && BMI < 15) {
-  console.log("Your heath is Very severely underweight");
-} else if (BMI >= 15 && BMI < 16) {
-  console.log("Your heath is Severely underweight");
-} else if (BMI >= 16 && BMI < 18.5) {
-  console.log("Your heath is Underweight");
-} else if (BMI >= 18.5 && BMI < 25) {
-  console.log("Your heath is Normal (healthy weight)");
-} else if (BMI >= 25 && BMI < 30) {
-  console.log("Your heath is Overweight");
-} else if (BMI >= 30 && BMI < 35) {
-  console.log("Your heath is Obese Class I (Moderately obese)");
-} else if (BMI >= 35 && BMI < 40) {
-  console.log("Your heath is Obese Class II (Severely obese)");
-} else if (BMI >= 40) {
-  console.log("Your heath is Obese Class III (Very severely obese)");
+switch (true) {
+  case BMI >= 0 && BMI < 15:
+    console.log("Your heath is Very severely underweight");
+    break;
+  case BMI >= 15 && BMI < 16:
+    console.log("Your heath is Severely underweight");
+    break;
+  case BMI >= 16 && BMI < 18.5:
+    console.log("Your heath is Underweight");
+    break;
+  case BMI >= 18.5 && BMI < 25:
+    console.log("Your heath is Normal (healthy weight)");
+    break;
+  case BMI >= 25 && BMI < 30:
+    console.log("Your heath is Overweight");
+    break;
+  case BMI >= 30 && BMI < 35:
+    console.log("Your heath is Obese Class I (Moderately obese)");
+    break;
+  case BMI >= 35 && BMI < 40:
+    console.log("Your heath is Obese Class II (Severely obese)");
+    break;
+  case BMI >= 40:
+    console.log("Your heath is Obese Class III (Very severely obese)");
+    break;
+  default:
+    console.log("Data is wrong");
 }
